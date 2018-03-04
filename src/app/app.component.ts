0e78e6a81c15442d50b794e82cf308e5ed45e884
Rohan Thorat <rthorat1@binghamton.edu>
2018-03-04 16:07:48 -0500

    excel data is read and displayed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,19 +7,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
     title = 'app';
+    public head;
+    public tableData = [];
 
     public changeListener(files: FileList){
       console.log(files);
       if(files && files.length > 0) {
         let file : File = files.item(0);
-        console.log(file.name);
-        console.log(file.size);
-        console.log(file.type);
         let reader: FileReader = new FileReader();
         reader.readAsText(file);
         reader.onload = (e) => {
           let csv: string = reader.result;
-          console.log(csv);
+          let allTextLines = csv.toString().split(/\r?\n|\r/);
+          let headers = allTextLines[0].split(',');
+          let lines = [];
+
+          for ( let i = 0; i < allTextLines.length; i++) {
+            // split content based on comma
+            let data = allTextLines[i].split(',');
+             let tarr = [];
+              for ( let j = 0; j < headers.length; j++) {
+                tarr.push(data[j]);
+              }
+              lines.push(tarr);
+          }
+          this.head = headers;
+          for (let k = 1; k < lines.length; k++) {
+            this.tableData.push(lines[k]);
+          }
         }
       }
     }
