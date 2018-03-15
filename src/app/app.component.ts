174a06f3e35e9192764f90d0b7496542751181e0
Rohan Thorat <rthorat1@binghamton.edu>
2018-03-15 15:17:19 -0400

    code refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css',
+              '../../node_modules/@progress/kendo-theme-default/dist/all.css'
+  ]
 })
 export class AppComponent {
     title = 'app';
     public head;
     public tableData = [];
+
+    onButtonClick() {
+    this.title = 'Hello from Kendo UI!';
+  }
 
     public changeListener(files: FileList){
       console.log(files);
@@ -19,7 +26,8 @@ export class AppComponent {
         reader.onload = (e) => {
           let csv: string = reader.result;
           let allTextLines = csv.toString().split(/\r?\n|\r/);
-          let headers = allTextLines[0].split(',');
+          let headers = allTextLines[0].split(
+            ',');
           let lines = [];
 
           for ( let i = 0; i < allTextLines.length; i++) {
